9cc58361caf79070efd2a439ffb0de27431ab46b
Maxim <32475421+MaximAfanasenko@users.noreply.github.com>
2024-04-19 19:34:35 +0700

    added base bottom sheet

--- lib/features/profile/profile_view.dart
+++ lib/features/profile/profile_view.dart
@@ -110,7 +110,33 @@ class _ProfileViewState extends State<ProfileView> {
                         height: 56,
                         child: ElevatedButton(
                           onPressed: () => {
-                            context.setLocale(const Locale('en', 'EN')),
+                            showModalBottomSheet<void>(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Container(
+                                  height: 200,
+                                  color: Colors.amber,
+                                  child: Center(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        const Text('Modal BottomSheet'),
+                                        ElevatedButton(
+                                          child:
+                                              const Text('Close BottomSheet'),
+                                          onPressed: () =>
+                                              Navigator.pop(context),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              },
+                            )
+
+                            //context.setLocale(const Locale('en', 'EN')),
                           },
                           child: const Text("Английский"),
                         ),
@@ -130,6 +156,7 @@ class _ProfileViewState extends State<ProfileView> {
                     ],
                   ),
                 ),
+                
               );
             },
           );
